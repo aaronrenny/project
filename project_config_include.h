eb68f5cfa1b01e24a9253dc374319ccc559ec23d
aaronrenny <91256112+aaronrenny@users.noreply.github.com>
2021-09-30 13:43:18 +0530

    Create project_config_include.h

---
+++ project_config_include.h
@@ -0,0 +1,17 @@
+/*
+ * aaron.h
+ *
+ *  Created on: Sep 28, 2021
+ *      Author: Aaron.renny
+ */
+
+#ifndef AARON_H_
+#define AARON_H_
+
+
+#include"configurations.h"
+
+
+
+
+#endif /* AARON_H_ */
